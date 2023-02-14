09c66473d82da3ab3f330cee01c694deb1425f07
Sachin Singla <ssingla@redhat.com>
2023-02-14 13:38:19 +0530

    [RFR] automated WINDUP-3322 + added supporting functions

--- cypress/e2e/models/analysis_configuration.ts
+++ cypress/e2e/models/analysis_configuration.ts
@@ -1,15 +1,26 @@
 import {
     clickByText,
     enableOption,
+    importFile,
     inputText,
     navigateTo,
     selectProject,
     shouldBeEnabled,
 } from "../../utils/utils";
-import { advancedOptionsPage, analysisConfiguration, next, save, SEC } from "../types/constants";
+import {
+    addRule,
+    advancedOptionsPage,
+    analysisConfiguration,
+    close,
+    customRules,
+    next,
+    runAnalysisButton,
+    save,
+    SEC,
+} from "../types/constants";
 import { advancedOptionsData } from "../types/types";
-import { pageTab, primaryButton } from "../views/common.view";
-import { inputSources } from "../views/projects.view";
+import { pageTab, primaryButton, secondaryButton } from "../views/common.view";
+import { enableRuleSwitch, inputSources } from "../views/projects.view";
 
 export class AnalysisConfiguration {
     projectName: string;
@@ -25,6 +36,27 @@ export class AnalysisConfiguration {
     //TODO: Add function to modify packages
 
     //TODO: Add function to modify rules
+    addNewRule(rule: string): void {
+        clickByText(pageTab, customRules);
+        cy.wait(10 * SEC);
+        cy.contains("h5", customRules, { timeout: 120 * SEC });
+        if (rule) {
+            clickByText(primaryButton, addRule);
+            importFile(rule);
+            clickByText(primaryButton, close);
+            cy.get(enableRuleSwitch).each(($switch) => {
+                cy.wrap($switch).click({ force: true });
+                cy.wait(SEC);
+            });
+        }
+    }
+
+    runAnalysis(): void {
+        cy.wait(10 * SEC);
+        shouldBeEnabled(secondaryButton, runAnalysisButton);
+        clickByText(secondaryButton, runAnalysisButton);
+        cy.wait(10 * SEC);
+    }
 
     //TODO: Add function to modify labels
 
